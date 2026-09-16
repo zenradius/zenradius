c711e137ae252bbcf90fa8c1d7e679865dd5f8a6
zenradius <zenradius.net@gmail.com>
2026-09-16 08:05:25 +0700

    fix(payment): whitelist webhook_secret as sensitive field so encryption/decryption on key read works properly; bump V.1.0.25

--- config/settingsEncryption.js
+++ config/settingsEncryption.js
@@ -32,6 +32,7 @@ const SENSITIVE_FIELDS = [
   'tripay_private_key',
   'midtrans_server_key',
   'telegram_bot_token',
+  'webhook_secret',
   'xendit_api_key',
   'duitku_api_key',
   'session_secret'
